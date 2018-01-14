e48d71b6655fef7a19cc4fe5b664e0155eed4e8f
Dawson M <dawson.mortenson@gmail.com>
2018-01-13 20:11:37 -0800

    create an image struct and begin defining methods to create an identicon

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -1,4 +1,24 @@
 defmodule Identicon do
+  def main(input) do
+    input
+    |> hash_input
+    |> pick_color
+  end
+
+  def pick_color(image) do
+    %Identicon.Image{ hex:  [r, g, b | _tail ] } = image
+
+    %Identicon.Image{ image | colors: { r, g, b } }
+  end
+
+  def hash_input(input) do
+    hex = :crypto.hash(:md5, input)
+    |> :binary.bin_to_list
+
+    %Identicon.Image{ hex: hex }
+  end 
+  
+  
   @moduledoc """
   Documentation for Identicon.
   """
