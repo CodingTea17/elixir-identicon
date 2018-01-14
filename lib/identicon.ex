abda11c20f2ebc6e319b43714802dd9dd703b66c
Dawson M <dawson.mortenson@gmail.com>
2018-01-13 20:38:43 -0800

    begin to build out methods to generate a grid

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -3,11 +3,12 @@ defmodule Identicon do
     input
     |> hash_input
     |> pick_color
+    |> build_grid
   end
 
-  def pick_color(image) do
-    %Identicon.Image{ hex:  [r, g, b | _tail ] } = image
-
+  # Pattern match r, g, b from image as it is recieved as input to the method. Weird. 
+  def pick_color(%Identicon.Image{ hex:  [r, g, b | _tail ] } = image) do
+    # REBUILD THE STRUCT AND ADD A TUPLE OF COLORS (R, G, B)
     %Identicon.Image{ image | colors: { r, g, b } }
   end
 
@@ -18,6 +19,15 @@ defmodule Identicon do
     %Identicon.Image{ hex: hex }
   end 
   
+  def build_grid(%Identicon.Image{ hex: hex } = image) do
+    hex
+    |> Enum.chunk(3)
+    |> Enum.map(&mirror_row/1)
+  end
+
+  def mirror_row([first, second | _tail] = row) do
+    row ++ [second, first]
+  end
   
   @moduledoc """
   Documentation for Identicon.
