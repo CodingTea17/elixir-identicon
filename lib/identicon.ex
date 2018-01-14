73939cb1076144eb5bb0e927b5cf576f6e1386be
Dawson M <dawson.mortenson@gmail.com>
2018-01-13 21:01:46 -0800

    add a grid value to the struct and create a method to filter out non-square grid values

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -4,6 +4,15 @@ defmodule Identicon do
     |> hash_input
     |> pick_color
     |> build_grid
+    |> filter_odd_squares
+  end
+  
+  def filter_odd_squares(%Identicon.Image{ grid: grid} = image) do
+    Enum.filter grid, fn({ code, _index }) -> 
+      rem(code, 2) == 0
+    end
+
+    %Identicon.Image{ image | grid: grid }
   end
 
   # Pattern match r, g, b from image as it is recieved as input to the method. Weird. 
@@ -20,9 +29,14 @@ defmodule Identicon do
   end 
   
   def build_grid(%Identicon.Image{ hex: hex } = image) do
-    hex
-    |> Enum.chunk(3)
-    |> Enum.map(&mirror_row/1)
+    grid = 
+      hex
+      |> Enum.chunk(3)
+      |> Enum.map(&mirror_row/1)
+      |> List.flatten
+      |> Enum.with_index
+
+    %Identicon.Image{ image | grid: grid }
   end
 
   def mirror_row([first, second | _tail] = row) do
